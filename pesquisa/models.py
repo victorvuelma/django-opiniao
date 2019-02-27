9782ea7ef8d451349433193ffabf308d8bb4c3bc
victorvuelma <victorvuelma@outlook.com>
2019-02-27 20:09:23 -0300

    Built the migrations for pesquisa.

--- pesquisa/models.py
+++ pesquisa/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Pergunta(models.Model):
+    texto = models.CharField(max_length=300)
+    data = models.DateTimeField(default=timezone.now)
+
+class Alternativa(models.Model):
+    pergunta = models.ForeignKey(Pergunta, models.CASCADE)
+    texto = models.CharField(max_length=150)
+    votos = models.IntegerField(default=0)
